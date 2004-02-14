0b0aea1e22e5329035dfbe60a41d59aed283aaa4
Dustin Graves <dgraves@computer.org>
2004-02-14 22:26:02 +0000

    Reorganization

---
+++ bootstrap.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+(automake --version) < /dev/null > /dev/null 2>&1 || 
+{
+	echo;
+	echo "You must have automake installed";
+	echo;
+	exit;
+}
+
+(autoconf --version) < /dev/null > /dev/null 2>&1 || 
+{
+	echo;
+	echo "You must have autoconf installed";
+	echo;
+	exit;
+}
+
+echo "Generating configuration files for fxcd...."
+echo;
+
+exec_cmd()
+{
+	echo executing $* ...;
+	if ! $*; then
+		echo "failed!";
+		exit;
+	fi
+}
+
+exec_cmd aclocal;
+exec_cmd automake --add-missing --copy;
+exec_cmd autoconf;
+
